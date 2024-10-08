8bb848a7cab9ace30cae82e20de324f3413c721f
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-10-08 13:04:43 +0000

    Add env.py and secret key

--- doggroomers/settings.py
+++ doggroomers/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rosewz+*(ass1$u%k47n-b_-n3^fs7lddgw&ocd=bke9-*s#4('
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-rosewz+*(ass1$u%k47n-b_-n3^fs7lddgw&ocd=bke9-*s#4('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
