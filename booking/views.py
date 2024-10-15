cd102e04e77dfaf2d3c3b1e69e8080dfa37e42f8
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-10-15 14:00:30 +0000

    Add static folder, style.css and some basic style to view on mobile and desktop

--- booking/views.py
+++ booking/views.py
@@ -8,12 +8,13 @@ from .models import Booking
 #     return HttpResponse('Welcome to the home page.')
 def home(request):
     return render(request, "booking/index.html")
-
+    
 
 class BookingList(generic.ListView):
     # model = Booking
     queryset = Booking.objects.all()
     template_name = "booking_list.html"
+    
 
 
 
