789619653297722d83556d9eaeb7bed471193c39
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-11-04 21:31:06 +0000

    Add styling to increase text size in Services section

--- booking/urls.py
+++ booking/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('profile', views.profile, name='profile_booking'),
     path('booking/', views.booking_page, name='booking_page'),  
     path('booking/<int:bk_id>/update', views.update_view, name='update_url'),
-    path('booking/<int:bk_id>/delete', views.delete_view, name='delete_url'),
+    path('booking/<int:bk_id>/delete', views.delete_view, name='delete_url'),      
 ]
